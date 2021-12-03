a17c6098eda5d70f2fc60ebfbe5241991270934c
Ragados <ragados@gmail.com>
2021-12-02 20:50:02 -0800

    Third method scripted.

--- main.py
+++ main.py
@@ -22,8 +22,10 @@ def guess_game(target):
         # Machine returns Guess number and higher, lower or Correct!
         else:
             guess_num = int(guess)
+            # Check input number within stated bounds
             if guess_num > 99:
                 print("Guess outside of stated range. Please try again.")
+            # Compare input number to target number
             elif guess_num == target:
                 print("Guess " + guess + " is Correct!")
                 break
@@ -38,16 +40,40 @@ def guess_game_good(target):
         # User Inputs number
         guess = input("Enter your guess integer number between 0 and 99: ")
         # Data entry validation - Had trouble with the escaping here.
-        # This is a longwinded way of doing .isnumeric() & is.decimal() checks
-        numerals = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
-        for char in range(len(guess)):
-            if guess[char] not in numerals:
-                print("Entry is not a valid number. Please try again.")
-                continue
-        # Machine returns Guess number and higher, lower or Correct!
-        guess_num = int(guess)
+        # Replacing the original solution with .isnumeric() check
+        if guess.isnumeric():
+            guess_num = int(guess)
+            # Check input number within stated bounds
+            if guess_num > 99:
+                print("Guess outside of stated range. Please try again.")
+            # Compare input number to target number
+            elif guess_num == target:
+                print("Guess " + guess + " is Correct!")
+                break
+            elif guess_num > target:
+                print("Guess " + guess + " is High")
+            else:
+                print("Guess " + guess + " is Low")
+        else:
+            print("Entry is not a valid number. Please try again.")
+
+
+def guess_game_better(target):
+    while True:
+        # User Inputs number
+        guess = input("Enter your guess integer number between 0 and 99: ")
+        # Data entry validation - Had trouble with the escaping here.
+        # Probably the most 'correct' method of acting.
+        # This is explicitly catching the error and moving past it.
+        try:
+            guess_num = int(guess)
+        except ValueError as err:
+            print('Input error:', err)
+            continue
+        # Check input number within stated bounds
         if guess_num > 99:
             print("Guess outside of stated range. Please try again.")
+        # Compare input number to target number
         elif guess_num == target:
             print("Guess " + guess + " is Correct!")
             break
@@ -59,4 +85,4 @@ def guess_game_good(target):
 
 print(target_num)
 # guess_game(target_num)
-guess_game_good(target_num)
+guess_game_better(target_num)
